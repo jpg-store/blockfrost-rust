3722f29e0fe3c9b3e2a8ea5be5c9e3230e613a4b
Joaquin Hoyos <clark@jpg.store>
2023-08-04 16:26:55 -0300

    add missing TransactionUtxosInput field

--- src/api/endpoints/transactions.rs
+++ src/api/endpoints/transactions.rs
@@ -267,6 +267,8 @@ pub struct TransactionUtxosInput {
     pub output_index: Integer,
     /// The hash of the transaction output datum.
     pub data_hash: Option<String>,
+    /// The inline datum of the transaction datum.
+    pub inline_datum: Option<String>,
     // Whether the input is a collateral consumed on script validation failure.
     pub collateral: bool,
 }
