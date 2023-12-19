e07fd40947957098068fd8a18a3570580321996d
Joaquin Hoyos <joaquin@Joaquins-MBP.fibertel.com.ar>
2023-12-19 16:06:38 -0300

    fix: wrong resolved conflicts

--- src/api/endpoints/addresses.rs
+++ src/api/endpoints/addresses.rs
@@ -110,7 +110,6 @@ mod tests {
         serde_json::from_value::<AddressContentTotal>(json_value).unwrap();
     }
 
-<<<<<<< HEAD
     #[tokio::test]
     async fn test_address_utxo() {
         let json_value = json!(  [
@@ -129,42 +128,6 @@ mod tests {
               }
             ],
             "block": "7eb8e27d18686c7db9a18f8bbcfe34e3fed6e047afaa2d969904d15e934847e6"
-=======
-    test_example! { test_address_utxo, Vec<AddressUtxo>, r#"
-    [
-      {
-        "tx_hash": "39a7a284c2a0948189dc45dec670211cd4d72f7b66c5726c08d9b3df11e44d58",
-        "address": "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz",
-        "output_index": 0,
-        "amount": [
-          {
-            "unit": "lovelace",
-            "quantity": "42000000"
-          }
-        ],
-        "block": "7eb8e27d18686c7db9a18f8bbcfe34e3fed6e047afaa2d969904d15e934847e6"
-      },
-      {
-        "tx_hash": "4c4e67bafa15e742c13c592b65c8f74c769cd7d9af04c848099672d1ba391b49",
-        "address": "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz",
-        "output_index": 0,
-        "amount": [
-          {
-            "unit": "lovelace",
-            "quantity": "729235000"
-          }
-        ],
-        "block": "953f1b80eb7c11a7ffcd67cbd4fde66e824a451aca5a4065725e5174b81685b7"
-      },
-      {
-        "tx_hash": "768c63e27a1c816a83dc7b07e78af673b2400de8849ea7e7b734ae1333d100d2",
-        "address": "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz",
-        "output_index": 1,
-        "amount": [
-          {
-            "unit": "lovelace",
-            "quantity": "42000000"
->>>>>>> 0e30a54 (fix: test)
           },
           {
             "address": "hm2",
